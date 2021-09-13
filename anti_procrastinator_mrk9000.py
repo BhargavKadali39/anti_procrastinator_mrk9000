f28d7adca4257713c90932694b65e52a820f5f5f
Bhargav <bhargavkadali39@gmail.com>
2021-09-13 19:29:28 +0530

    Update anti_procrastinator_mrk9000.py

--- anti_procrastinator_mrk9000.py
+++ anti_procrastinator_mrk9000.py
@@ -1,1 +1,19 @@
+import string
+import random
+i = 0
+while i <= 1:
+    if (i == 0):
+        chs = random.choice( [ True,False ] )
+        if(chs == True):
+            coin = 'Heads'
+        else:
+            coin = 'Tails'
+        print("It's " + coin)
+        
+        cond = str(input('\nDo u want to flip again ?\n Type "y/n"\n->').lower())
+        if cond == 'y':
+            pass
+        else:
+            print('Gracias')
+            i = 2
 
